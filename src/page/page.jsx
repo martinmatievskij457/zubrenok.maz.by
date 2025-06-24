67ddd0d6e9bb6c37d10a9bbf6c4e5d8e669c005b
Martin Matievskiy <29281028873martin@gmail.com>
2025-06-24 16:17:31 +0300

    Adaptive design for the fifth  section of the site has been made.

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -19,8 +19,8 @@ function Page() {
             <Races />
             <About />
             <Routine />
-            {/* <Tentcamp />
-            <Questions />
+            <Tentcamp />
+            {/* <Questions />
             <News /> */}
           </div>
         </main>
--- src/page/sections/tentcamp/tentcamp.jsx
+++ src/page/sections/tentcamp/tentcamp.jsx
@@ -2,49 +2,57 @@ import "./tentcamp.css";
 import picture from "../../../assets/images/picture.png";
 import tent from "../../../assets/images/tent.png";
 import bag from "../../../assets/images/bag.png";
-import rectangle1 from "../../../assets/images/rectangle1.png";
+import curvedLine from "../../../assets/images/Rectangle11.png";
 import icon from "../../../assets/images/icon_download.png";
 
 function Tentcamp() {
   return (
     <div className="tentcamp" id="tentcamp">
-      <div className="tentcamp_content">
-        <div className="content_wrapper">
-          <div className="tentcamp_title">
-            <div className="tentcamp_title_text">Палаточный лагерь</div>
-            <div className="tentcamp_title_info">
+      <div className="tentcamp__content">
+        <div className="tentcamp__title-wrapper">
+          <div className="tentcamp__title">
+            <div className="tentcamp__title__text">Палаточный лагерь</div>
+            <div className="tentcamp__title__info">
               ОЛ «Зубренок» д. Волма приглашает детей 12-17 лет в профильный
               лагерь с использованием палаток.
             </div>
-            <div className="tentcamp_title_info">
+            <div className="tentcamp__title__info">
               Период оздоровления – 9 дней.
             </div>
-            <div className="tentcamp_title_button">
-              <div className="button_text">План мероприятия</div>
-              <div className="button_icon">
+            <div className="tentcamp__title__button">
+              <div className="button__text">План мероприятия</div>
+              <div className="button__icon">
                 <img src={icon} alt="Icon Download" />
               </div>
             </div>
           </div>
-          <div className="img_wrapper">
-            <div className="img_tentcamp">
-              <img src={picture} alt="Picture" />
+        </div>
+        <div className="tentcamp__images">
+          <div className="tentcamp__sign-wrapper">
+            <div className="tentcamp__sign">
+              <div className="tentcamp__sign__image">
+                <img src={picture} alt="Picture" className="image__sign"/>
+              </div>
             </div>
           </div>
-          <div className="tent_wrapper">
-            <div className="tent_img">
-              <img src={tent} alt="Tent" />
-            </div>
-            <div className="bag_img">
-              <img src={bag} alt="Bag" />
+          <div className="tentcamp__tent-wrapper">
+            <div className="tentcamp__tent">
+              <div className="tent__image-wrapper">
+                <div className="tent__image">
+                  <img src={tent} alt="Tent" className="image__tent" />
+                </div>
+              </div>
+              <div className="bag__image-wrapper">
+                <div className="bag__image">
+                  <img src={bag} alt="Bag" className="image__bag" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="line_wrapper">
-          <div className="line_img">
-            <img src={rectangle1} alt="Line" />
-          </div>
-        </div>
+      </div>
+      <div className="tentcamp__curved">
+        <img src={curvedLine} alt="Curved Line" />
       </div>
     </div>
   );
